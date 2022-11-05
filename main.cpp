4c8b62b6626c2e4f7303f3ffea479579b8e78d6a
Michał Żuk <michal.zuk30601@gmail.com>
2022-11-05 22:50:45 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*
+Projekt i implementacja programu obliczającego całkę złożoną metodą trapezów, Michał Żuk 87604
+*/
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"Polish");
+    return 0;
+}
